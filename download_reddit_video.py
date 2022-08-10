2781b1693513608f6509f7048c38e299b9eb0af4
Fenrir7734 <hitex1999@gmail.com>
2022-08-10 15:36:48 +0200

    Downloading file in chunks and added progress bar

--- download_reddit_video.py
+++ download_reddit_video.py
@@ -4,6 +4,7 @@ import json
 import re
 import ffmpeg
 import os
+from tqdm import tqdm
 
 USER_AGENT_HEADER = {"User-agent": "Mozilla/5.0"}
 
@@ -16,19 +17,20 @@ def download(reddit_post_url: str) -> None:
         print("Post does not contains video")
         sys.exit()
 
+    print("\nDownloading video file...")
     video_url = get_video_url(json_data)
-    res = make_request(video_url)
-    video_title = get_post_name(json_data) + "_video." + res.headers["Content-Type"][6:]
-    open(video_title, "wb").write(res.content)
+    video_title = get_post_name(json_data) + "_video.mp4"
+    download_file(video_url, video_title)
 
+    print("\nDownloading audio file...")
     audio_url = get_audio_url(json_data)
-    res = make_request(audio_url)
-    audio_title = get_post_name(json_data) + "_audio." + res.headers["Content-Type"][6:]
-    open(audio_title, "wb").write(res.content)
+    audio_title = get_post_name(json_data) + "_audio.mp4"
+    download_file(audio_url, audio_title)
 
+    print("\nMerging video and audio...")
     input_video = ffmpeg.input(video_title)
     input_audio = ffmpeg.input(audio_title)
-    output_title = get_post_name(json_data) + "." + res.headers["Content-Type"][6:]
+    output_title = get_post_name(json_data) + ".mp4"
     ffmpeg.concat(input_video, input_audio, v=1, a=1).output(output_title, loglevel="quiet").run()
 
     os.remove(video_title)
@@ -43,6 +45,16 @@ def make_request(url: str) -> requests.Response:
 
     print(f"Failed to download video. Http status code: {res.status_code}")
     sys.exit()
+
+
+def download_file(url: str, out_file_name: str) -> None:
+    res = requests.get(url, headers=USER_AGENT_HEADER, stream=True)
+    with open(out_file_name, 'wb') as f:
+        pbar = tqdm(unit="B", total=int(res.headers['Content-Length']))
+        for chunk in res.iter_content(chunk_size=1024):
+            if chunk:
+                pbar.update(len(chunk))
+                f.write(chunk)
 
 
 def is_video(json_data) -> bool:
