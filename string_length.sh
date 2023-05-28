8e93dbbce3bc88f76ad18e3a1871217ddc75a586
Fenrir7734 <hitex1999@gmail.com>
2023-05-28 14:43:15 +0200

    Added count for words, spaces and digits

--- string_length.sh
+++ string_length.sh
@@ -2,4 +2,14 @@
 
 [[ -z $1 ]] && echo "You need to provide string as an argument" && exit 1
 
-echo $1 | awk '{print length}'
+input=$1
+
+chars=$(echo -n "$input" | wc -c)
+words=$(echo -n "$input" | wc -w)
+spaces=$(echo -n "$input" | tr -d -c " " | wc -m)
+digits=$(echo -n "$input" | sed "s/[^0-9]//g" | wc -c)
+
+printf "%-15s %s\n" "Chars" "$chars"
+printf "%-15s %s\n" "Words" "$words"
+printf "%-15s %s\n" "Spaces" "$spaces"
+printf "%-15s %s\n" "Digits" "$digits"
